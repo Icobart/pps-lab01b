8fe4262e21c1f146f749360dccce471f0f827166
Icobart <enrico.cornacchia@studio.unibo.it>
2026-03-04 00:04:35 +0100

    correctly integrated new Pawn class into LogicsImpl

--- src/main/java/it/unibo/pps/e2/LogicsImpl.java
+++ src/main/java/it/unibo/pps/e2/LogicsImpl.java
@@ -4,27 +4,27 @@ import java.util.*;
 
 public class LogicsImpl implements Logics {
 	
-	private final Pair<Integer,Integer> pawn;
+	private final Piece pawn;
 	private Piece knight;
 	private final Random random = new Random();
 	private final int size;
 	 
     public LogicsImpl(int size){
     	this.size = size;
-        this.pawn = this.randomEmptyPosition();
+        this.pawn = new Pawn(this.randomEmptyPosition());
         this.knight = new Knight(this.randomEmptyPosition());
     }
 
     public LogicsImpl(int size, Pair<Integer, Integer> initialPawnPosition, Pair<Integer, Integer> initialKnightPosition) {
         this.size = size;
-        this.pawn = initialPawnPosition;
+        this.pawn = new Pawn(initialPawnPosition);
         this.knight = new Knight(initialKnightPosition);
     }
     
 	private final Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
     	// the recursive call below prevents clash with an existing pawn
-    	return this.pawn!=null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
+    	return this.pawn!=null && this.pawn.getPosition().equals(pos) ? randomEmptyPosition() : pos;
     }
     
 	@Override
@@ -36,7 +36,7 @@ public class LogicsImpl implements Logics {
         Pair<Integer, Integer> newPosition = new Pair<>(row, col);
 		if (this.knight.canMove(newPosition)) {
 			this.knight.move(newPosition);
-			return this.pawn.equals(this.knight.getPosition());
+			return this.pawn.getPosition().equals(this.knight.getPosition());
 		}
 		return false;
 	}
@@ -48,6 +48,6 @@ public class LogicsImpl implements Logics {
 
 	@Override
 	public boolean hasPawn(int row, int col) {
-		return this.pawn.equals(new Pair<>(row,col));
+		return this.pawn.getPosition().equals(new Pair<>(row,col));
 	}
 }
