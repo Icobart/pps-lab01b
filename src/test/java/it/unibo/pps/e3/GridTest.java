19353344cf9815ea335e66f2b90935130302dd26
Icobart <enrico.cornacchia@studio.unibo.it>
2026-03-05 15:12:40 +0100

    working test to check correct initialization of empty cells on the grid

--- src/test/java/it/unibo/pps/e3/GridTest.java
+++ src/test/java/it/unibo/pps/e3/GridTest.java
@@ -6,15 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GridTest {
 
+    public static final int SIZE = 3;
     private Grid grid;
 
     @Test
     public void testMineCellCorrectlyInitialized() {
-        this.grid = new GridImpl(3, List.of(new Pair<>(1, 1)));
+        this.grid = new GridImpl(SIZE, List.of(new Pair<>(1, 1)));
         assertTrue(this.grid.getCell(new Pair<>(1, 1)).isMine());
+    }
+
+    @Test
+    public void testEmptyCellCorrectlyInitialized() {
+        this.grid = new GridImpl(SIZE, List.of(new Pair<>(1, 1)));
+        assertFalse(this.grid.getCell(new Pair<>(0, 0)).isMine());
     }
 }
