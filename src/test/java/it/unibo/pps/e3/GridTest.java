4eeecdf41baaa341defbc8a7ddab939ebc2603dd
Icobart <enrico.cornacchia@studio.unibo.it>
2026-03-05 20:52:49 +0100

    refactor of magic numbers in test classes

--- src/test/java/it/unibo/pps/e3/GridTest.java
+++ src/test/java/it/unibo/pps/e3/GridTest.java
@@ -10,51 +10,60 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GridTest {
 
     public static final int SIZE = 3;
+    public static final Pair<Integer, Integer> CENTER_POSITION = new Pair<>(1, 1);
+    public static final Pair<Integer, Integer> CORNER_POSITION = new Pair<>(0, 0);
+    public static final Pair<Integer, Integer> EDGE_POSITION = new Pair<>(0, 1);
+    public static final Pair<Integer, Integer> OPPOSITE_CORNER_CORNER = new Pair<>(2, 2);
     private Grid grid;
 
     @Test
     public void testMineCellCorrectlyInitialized() {
-        this.grid = new GridImpl(SIZE, List.of(new Pair<>(1, 1)));
-        assertTrue(this.grid.getCell(new Pair<>(1, 1)).isMine());
+        this.grid = new GridImpl(SIZE, List.of(CENTER_POSITION));
+        assertTrue(this.grid.getCell(CENTER_POSITION).isMine());
     }
 
     @Test
     public void testEmptyCellCorrectlyInitialized() {
-        this.grid = new GridImpl(SIZE, List.of(new Pair<>(1, 1)));
-        assertFalse(this.grid.getCell(new Pair<>(0, 0)).isMine());
+        this.grid = new GridImpl(SIZE, List.of(CENTER_POSITION));
+        assertFalse(this.grid.getCell(CORNER_POSITION).isMine());
     }
 
     @Test
     public void testAdjacentPositionsForCenterCell() {
-        this.grid = new GridImpl(3, List.of());
-        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(new Pair<>(1, 1));
-        assertEquals(8, neighbours.size());
+        this.grid = new GridImpl(SIZE, List.of());
+        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(CENTER_POSITION);
+        int expectedAdjacentCells = 8;
+        assertEquals(expectedAdjacentCells, neighbours.size());
     }
 
     @Test
     public void testAdjacentPositionsForCornerCell() {
-        this.grid = new GridImpl(3, List.of());
-        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(new Pair<>(0, 0));
-        assertEquals(3, neighbours.size());
+        this.grid = new GridImpl(SIZE, List.of());
+        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(CORNER_POSITION);
+        int expectedAdjacentCells = 3;
+        assertEquals(expectedAdjacentCells, neighbours.size());
     }
 
     @Test
     public void testAdjacentPositionsForEdgeCell() {
-        this.grid = new GridImpl(3, List.of());
-        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(new Pair<>(0, 1));
-        assertEquals(5, neighbours.size());
+        this.grid = new GridImpl(SIZE, List.of());
+        List<Pair<Integer, Integer>> neighbours = this.grid.getAdjacentPositions(EDGE_POSITION);
+        int expectedAdjacentCells = 5;
+        assertEquals(expectedAdjacentCells, neighbours.size());
     }
 
     @Test
     public void testAdjacentMinesShouldBeZeroWhenNearbyThereAreNone() {
-        this.grid = new GridImpl(SIZE, List.of(new Pair<>(0, 0)));
-        assertEquals(0, this.grid.countAdjacentMines(new Pair<>(2, 2)));
+        this.grid = new GridImpl(SIZE, List.of(CORNER_POSITION));
+        int expectedMinesNumber = 0;
+        assertEquals(expectedMinesNumber, this.grid.countAdjacentMines(OPPOSITE_CORNER_CORNER));
     }
 
     @Test
     public void testAdjacentMinesShouldBeNonZeroWhenNearbyThereAreAtLeastOne() {
-        this.grid = new GridImpl(SIZE, List.of(new Pair<>(0, 0), new Pair<>(0, 1), new Pair<>(0, 2)));
-        assertEquals(3, this.grid.countAdjacentMines(new Pair<>(1, 1)));
+        this.grid = new GridImpl(SIZE, List.of(CORNER_POSITION, EDGE_POSITION, new Pair<>(0, 2)));
+        int expectedMines = 3;
+        assertEquals(expectedMines, this.grid.countAdjacentMines(CENTER_POSITION));
     }
 
 }
--- src/test/java/it/unibo/pps/e3/LogicTest.java
+++ src/test/java/it/unibo/pps/e3/LogicTest.java
@@ -8,54 +8,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
 
+    public static final int DEFAULT_TEST_GRID_SIZE = 3;
+    public static final Pair<Integer, Integer> TOP_LEFT_MINE = new Pair<>(0, 0);
+    public static final Pair<Integer, Integer> BOTTOM_RIGHT_SAFE = new Pair<>(2, 2);
+    public static final Pair<Integer, Integer> TOP_RIGHT_SAFE = new Pair<>(0, 2);
+    public static final Pair<Integer, Integer> CENTER_SAFE = new Pair<>(1, 1);
+
     @Test
     public void testHitMineResultsInLoss() {
-        List<Pair<Integer, Integer>> mines = List.of(new Pair<>(1, 1));
-        Logics logics = new LogicsImpl(3, mines);
-        logics.hit(new Pair<>(1, 1));
+        List<Pair<Integer, Integer>> mines = List.of(CENTER_SAFE);
+        Logics logics = new LogicsImpl(DEFAULT_TEST_GRID_SIZE, mines);
+        logics.hit(CENTER_SAFE);
         assertTrue(logics.isLost());
     }
 
     @Test
     public void testHitEmptyCellShouldNotResultInLoss() {
-        List<Pair<Integer, Integer>> mines = List.of(new Pair<>(1, 1));
-        Logics logics = new LogicsImpl(3, mines);
-        logics.hit(new Pair<>(0, 0));
+        List<Pair<Integer, Integer>> mines = List.of(CENTER_SAFE);
+        Logics logics = new LogicsImpl(DEFAULT_TEST_GRID_SIZE, mines);
+        logics.hit(TOP_LEFT_MINE);
         assertFalse(logics.isLost());
     }
 
     @Test
     public void testHitCellWithZeroAdjacentMinesShouldTriggerCascade() {
-        List<Pair<Integer, Integer>> mines = List.of(new Pair<>(0, 0));
-        Logics logics = new LogicsImpl(3, mines);
-        logics.hit(new Pair<>(2, 2));
+        List<Pair<Integer, Integer>> mines = List.of(TOP_LEFT_MINE);
+        Logics logics = new LogicsImpl(DEFAULT_TEST_GRID_SIZE, mines);
+        logics.hit(BOTTOM_RIGHT_SAFE);
         assertAll(
-                () -> assertTrue(logics.isRevealed(new Pair<>(2, 2))),
-                () -> assertTrue(logics.isRevealed(new Pair<>(0, 2))),
-                () -> assertTrue(logics.isRevealed(new Pair<>(1, 1))),
-                () -> assertFalse(logics.isRevealed(new Pair<>(0, 0)))
+                () -> assertTrue(logics.isRevealed(BOTTOM_RIGHT_SAFE)),
+                () -> assertTrue(logics.isRevealed(TOP_RIGHT_SAFE)),
+                () -> assertTrue(logics.isRevealed(CENTER_SAFE)),
+                () -> assertFalse(logics.isRevealed(TOP_LEFT_MINE))
         );
     }
 
     @Test
     public void testHitCellWithAdjacentMinesShouldNotCascade() {
-        List<Pair<Integer, Integer>> mines = List.of(new Pair<>(0, 0));
-        Logics logics = new LogicsImpl(3, mines);
-        logics.hit(new Pair<>(0, 1));
+        List<Pair<Integer, Integer>> mines = List.of(TOP_LEFT_MINE);
+        Logics logics = new LogicsImpl(DEFAULT_TEST_GRID_SIZE, mines);
+        Pair<Integer, Integer> cellAdjacentMine = new Pair<>(0, 1);
+        logics.hit(cellAdjacentMine);
         assertAll(
-                () -> assertTrue(logics.isRevealed(new Pair<>(0, 1))),
-                () -> assertFalse(logics.isRevealed(new Pair<>(0, 2))),
-                () -> assertFalse(logics.isRevealed(new Pair<>(1, 1)))
+                () -> assertTrue(logics.isRevealed(cellAdjacentMine)),
+                () -> assertFalse(logics.isRevealed(TOP_RIGHT_SAFE)),
+                () -> assertFalse(logics.isRevealed(CENTER_SAFE))
         );
     }
 
     @Test
     public void testVictoryCondition() {
-        List<Pair<Integer, Integer>> mines = List.of(new Pair<>(0, 0));
-        Logics logics = new LogicsImpl(2, mines);
-        logics.hit(new Pair<>(0, 1));
-        logics.hit(new Pair<>(1, 1));
-        logics.hit(new Pair<>(1, 0));
+        List<Pair<Integer, Integer>> mines = List.of(TOP_LEFT_MINE);
+        int smallGridSize = 2;
+        Logics logics = new LogicsImpl(smallGridSize, mines);
+        Pair<Integer, Integer> firstCell = new Pair<>(0, 1);
+        logics.hit(firstCell);
+        logics.hit(CENTER_SAFE);
+        Pair<Integer, Integer> secondCell = new Pair<>(1, 0);
+        logics.hit(secondCell);
         assertTrue(logics.isWon());
     }
 }
